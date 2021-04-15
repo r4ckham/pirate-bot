8473d390c355671bb942ad638edf9227752d224a
Ribot Alexis <alexisribot10@gmail.com>
2021-04-15 19:43:14 +0200

    Add env variables

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ var app = express();
 app.set('port', (process.env.PORT || 3456));
 
 app.listen(app.get('port'), function () {
-    var nconf = require('nconf');
-    nconf.argv().env().file({file: 'config.json'});
-    // FIREBASE
+    const env = require('dotenv').config();
     const admin = require('firebase-admin');
     admin.initializeApp({
-        credential: admin.credential.cert('pirate-bot-e12c3-firebase-adminsdk-g1cyf-1c48e3f223.json')
+        credential: admin.credential.cert({
+            "type": process.env.TYPE,
+            "project_id": process.env.PROJECT_ID,
+            "private_key_id": process.env.PRIVATE_KEY_ID,
+            "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+            "client_email": process.env.CLIENT_EMAIL,
+            "client_id": process.env.CLIENT_ID,
+            "auth_uri": process.env.AUTH_URI,
+            "token_uri": process.env.TOKEN_URI,
+            "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+            "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
+         })    
     });
     const db = admin.firestore();
     // DISCORD
@@ -74,5 +83,5 @@ app.listen(app.get('port'), function () {
 
     });
 
-    client.login(nconf.get('BOT_TOKEN'));
+    client.login(process.env.BOT_TOKEN);
 });
